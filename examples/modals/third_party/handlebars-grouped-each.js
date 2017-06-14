c0f7e027e61c06af72c2312861d62c92966b37ba
Rob Dodson <robdodson@google.com>
2017-06-14 10:23:28 -0700

    Licenses

--- examples/modals/third_party/handlebars-grouped-each.js
+++ examples/modals/third_party/handlebars-grouped-each.js
@@ -1,5 +1,10 @@
-// Credit to https://funkjedi.com/technology/412-every-nth-item-in-handlebars/
+/**
+ * @license Copyright 2017 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
 
+// Credit to https://funkjedi.com/technology/412-every-nth-item-in-handlebars/
 Handlebars.registerHelper('grouped_each', function(every, context, options) {
   var out = "",
     subcontext = [],
